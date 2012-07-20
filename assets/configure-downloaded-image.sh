575151e4ae104004205f5798cba6ba53498df283
Hans-Christoph Steiner <hans@eds.org>
2012-07-20 12:59:17 -0400

    include app_bin in the PATH for ./configure-downloaded-image.sh

--- assets/configure-downloaded-image.sh
+++ assets/configure-downloaded-image.sh
@@ -3,6 +3,11 @@
 # see lildebi-common for arguments, the args are converted to vars there.  The
 # first arg is the "app payload" directory where the included scripts are kept
 
+# many phones don't even include 'test', so set the path to our
+# busybox tools first, where we provide all the UNIX tools needed by
+# this script
+export PATH=$1:/system/bin:/system/xbin:$PATH
+
 test -e $1/lildebi-common || exit
 . $1/lildebi-common
 
