853d31d1ae784e76f7f4faf32671bab14a4bad6e
Hans-Christoph Steiner <hans@eds.org>
2013-03-29 15:10:52 -0700

    add script for the release build process

---
+++ make-release-build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd external/busybox
+git reset --hard
+git clean -fdx
+cd ../..
+git reset --hard
+git clean -fdx
+git submodule init
+git submodule update
+make -C external/ assets
+
+./update-ant-build.sh
+ant release
+
+gpg --detach-sign bin/LilDebi-release-unsigned.apk
