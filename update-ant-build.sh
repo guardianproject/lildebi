900fb56af67e1aeafcdfd02de67595129b815e04
Hans-Christoph Steiner <hans@eds.org>
2014-04-09 14:16:01 -0400

    update reference to use official var: $ANDROID_HOME

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# make sure your Android SDK tools path is set in SDK_BASE
+# make sure your Android SDK tools path is set in ANDROID_HOME
 android update project --path . --name LilDebi
