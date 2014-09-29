37dfcfd4f06b6a1c859d8489255df065c80d9e92
SDkie <mesdkie@gmail.com>
2014-09-30 01:25:28 +0530

    check if Debian image is available after SDcard is unmounted
    as some devices has multiple SDcards

--- src/info/guardianproject/lildebi/MediaUnmountedReceiver.java
+++ src/info/guardianproject/lildebi/MediaUnmountedReceiver.java
@@ -9,10 +9,11 @@ public class MediaUnmountedReceiver extends BroadcastReceiver {
 	private LilDebiAction action;
 
 	public void onReceive(Context context, Intent intent) {
-		if (NativeHelper.isStarted()) {
+		// '!NativeHelper.isInstalled()' check tells us that the image is not
+		// installed after SDcard is unmounted
+		if (!NativeHelper.isInstalled() && NativeHelper.isStarted()) {
 			action = new LilDebiAction(context, null);
 			action.stopDebian();
 		}
 	}
 }
-
