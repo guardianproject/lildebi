a7b4bc0a81ff53065570c7227ceb66598465671f
SDkie <mesdkie@gmail.com>
2014-10-11 20:12:17 +0530

    added lildebi shutdown on ACTION_EJECT and ACTION_MEDIA_BUTTON
    added new MediaEjectReceiver.java for that
    
    closes #113 https://github.com/guardianproject/lildebi/issues/113

---
+++ src/info/guardianproject/lildebi/MediaEjectReceiver.java
@@ -0,0 +1,17 @@
+package info.guardianproject.lildebi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MediaEjectReceiver extends BroadcastReceiver {
+
+	private LilDebiAction action;
+
+	public void onReceive(Context context, Intent intent) {
+		if (!NativeHelper.installInInternalStorage && NativeHelper.isStarted()) {
+			action = new LilDebiAction(context, null);
+			action.stopDebian();
+		}
+	}
+}
