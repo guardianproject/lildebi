eab22691fa5be9fb8874c2e96bff1fc280c1e75d
SDkie <mesdkie@gmail.com>
2014-10-13 00:12:27 +0530

    add MEDIA_SHARED for MediaEjectReceiver

--- src/info/guardianproject/lildebi/MediaEjectReceiver.java
+++ src/info/guardianproject/lildebi/MediaEjectReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+// This receiver tries to stopDebian when sdcard is present
 public class MediaEjectReceiver extends BroadcastReceiver {
 
 	private LilDebiAction action;
